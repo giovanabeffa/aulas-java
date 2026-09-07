9cc4184e0769fc09980d61a41dcbc976896bbf4c
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-07 14:54:44 -0300

    Create ex05.java

---
+++ exercicios_2/src/ex05.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digeite um ano: ");
+        int ano = scanner.nextInt();
+
+        if (ano % 4 == 0 || ano % 400 == 0 && ano % 100 != 0) {
+            System.out.println(ano + " é um ano bissexto.");
+        } else {
+            System.out.println(ano + " não é um ano bissexto.");
+        }
+
+        scanner.close();
+    }
+}

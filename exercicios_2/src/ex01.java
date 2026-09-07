b292f9284fb628516d38557825bb2775eec64c7b
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-07 09:54:09 -0300

    Exercício 1

---
+++ exercicios_2/src/ex01.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite um núme inteiro:" );
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é ímpar.");
+        }
+    }
+}

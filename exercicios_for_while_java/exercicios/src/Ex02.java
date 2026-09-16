0ccf7f01ea25e4aa436653a6935d2a8bff7a2d35
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-16 19:31:28 -0300

    Create Ex02.java

---
+++ exercicios_for_while_java/exercicios/src/Ex02.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o número de trechos:");
+        trechos = scanner.nextInt();
+    }
+}

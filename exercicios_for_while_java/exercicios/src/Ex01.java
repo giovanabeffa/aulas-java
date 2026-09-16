f68dd97146ec048cb4797c00e03996e13a5da5d8
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-16 19:31:25 -0300

    Create Ex01.java

---
+++ exercicios_for_while_java/exercicios/src/Ex01.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o valor inicial da dívida:");
+        double divida = scanner.nextDouble();
+
+        System.out.println("Informe o a taxa de juros mensal:");
+        double juros = scanner.nextDouble();
+
+        System.out.println("Informe a quantidade de meses:");
+        int meses = scanner.nextInt();
+
+        for (int i = 1; i <= meses; i++) {
+            
+            divida = divida + (divida * (juros/100));
+
+            System.out.printf("Mês %d: R$ %.2f%n", i, divida);
+        }
+        
+        scanner.close();
+    }
+}

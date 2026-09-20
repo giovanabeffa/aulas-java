eae2e6172b158bee67e58e193bf68a66debb8bfa
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-20 10:10:42 -0300

    Update Ex07.java

--- exercicios_for_while_java/exercicios/src/Ex07.java
+++ exercicios_for_while_java/exercicios/src/Ex07.java
@@ -7,6 +7,7 @@ public class Ex07 {
 
         double valorVenda = 0;
         double valorTotal = 0;
+        int vendas = 0;
         int escolha;
         do {
             System.out.println("Digite o valor da venda:");
@@ -14,11 +15,16 @@ public class Ex07 {
 
             valorTotal += valorVenda;
 
+            vendas++;
+
             System.out.println("Deseja registrar outra venda?\n1 - Sim\n2- Não");
             escolha = scanner.nextInt();
             
         } while (escolha == 1);
+
+        double media = valorTotal / vendas;
         
         System.out.printf("Total vendido: R$ %.2f%n", valorTotal);
+        System.out.printf("Média das venda: R$ %.2f%n", media);
     }
 }

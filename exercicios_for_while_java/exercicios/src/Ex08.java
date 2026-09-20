3379d7a79d2d0abc883b79e3d4cb746f7626d9b6
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-20 10:54:39 -0300

    Update Ex08.java

--- exercicios_for_while_java/exercicios/src/Ex08.java
+++ exercicios_for_while_java/exercicios/src/Ex08.java
@@ -5,6 +5,28 @@ public class Ex08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Informe o valor inicial da dívida: R$ ");
+        double divida = scanner.nextDouble();
+
+        while (divida > 0) {
+            System.out.println("Informe o valor do pagamento: R$ ");
+            double pagamento = scanner.nextDouble();
+
+            if (pagamento > divida) {
+                double troco = pagamento - divida;
+                System.out.printf("Dívida quitada! Valor a ser devolvido: R$ %.2f%n", troco);
+                scanner.close();
+                return;
+            } else if (pagamento <= 0) {
+                System.out.println("O valor do pagamento não pode ser menor igual a R$ 0,00.");
+            } else {
+                divida -= pagamento;
+                System.out.printf("Pagamento: R$ %.2f - Saldo: R$ %.2f%n", pagamento, divida);
+            }
+        }
+
+        scanner.close();
+
+        System.out.println("Dívida quitada!");
     }
 }
